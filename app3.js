e6dccf7253aefa2088652f4036451533e7336438
sanchezmigben <82328456+sanchezmigben@users.noreply.github.com>
2022-02-08 12:13:51 +0100

    Update app3.js

--- app3.js
+++ app3.js
@@ -1,3 +1,35 @@
+const btnEdit = document.getElementById("btnEdit");
+btnEdit.addEventListener("click",fetchEditProduct);
+
+
+async function fetchEditProduct() {
+	const idField = document.getElementById("txtId").value;
+	const nameField = document.getElementById("txtName").value;
+	const priceField = document.getElementById("txtPrice").value;
+	const categoryField = document.getElementById("txtCategory").value;
+	console.log(nameField + " " + priceField + " " + categoryField);
+	
+	const newProduct = {name: nameField, price: priceField, category:categoryField};
+	
+    const response = await fetch(
+      "https://rest-atlas-test.herokuapp.com/products/idField?_method=PUT",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+		body: JSON.stringify(newProduct)
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        
+        
+      })
+      .catch((error) => console.log(error));
+  }
 
 
 async function fetchProduct(id) {
